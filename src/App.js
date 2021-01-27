6296c670e394de231667233c56aa7cc75aebb1c3
miriam said <miriamsaid.ms@gmail.com>
2021-01-27 15:47:11 +0000

    backgrounds

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import $ from "jquery";
+
+
+$(document).ready(function(){
+  var header = $('body');
+  
+  var backgrounds = new Array(
+    'url(desert.jpg)',
+    'url(country.jpg)',
+    'url(mountains.jpg)',
+    'url(orangesky.jpg)',
+    'url(trees.jpg)',
+    'url(beach.jpg)',
+  );
+  
+  var current = 0;
+  
+  function nextBackground() {
+      current++;
+      current = current % backgrounds.length;
+      header.css('background-image', backgrounds[current]);
+  }
+  setInterval(nextBackground, 1000);
+  
+  header.css('background-image', backgrounds[0]);
+  });
 
 function App() {
   return (
@@ -10,7 +36,7 @@ function App() {
       <main>
 
       </main>
-      <footer class="bg-light mt-auto py-3 text-center text-muted"> Coded by Miriam Said</footer>
+      <footer class="bg-light mt-auto py-3 text-center text-muted">- Coded by Miriam Said -</footer>
     </div>
   );
 }
