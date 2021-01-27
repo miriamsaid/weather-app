15bd233bce77bb7ebbc6d2e3a2d5c3205a5a73c1
miriam said <miriamsaid.ms@gmail.com>
2021-01-27 18:54:57 +0000

    layout

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-import $ from "jquery";
-
-
-var images = ["desert.jpg", "country.jpg", "mountains.jpg", "orangesky.jpg", "trees.jpg", "beach.jpg"];
-
-    $(function () {
-        var i = 0;
-        $("#dvImage").css("background-image", "url(images/" + images[i] + ")");
-        setInterval(function () {
-            i++;
-            if (i == images.length) {
-                i = 0;
-            }
-            $("#dvImage").fadeOut("slow", function () {
-                $(this).css("background-image", "url(images/" + images[i] + ")");
-                $(this).fadeIn("slow");
-            });
-        }, 5000);
-    });
+const api = {
+  key: "cf1a610a47c7a1e0c56e3391803c790f",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
-  return (
-    <div id="dvImage" className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <main>
 
+  const dateBuilder = (d) => {
+    let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"]
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",]
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()]
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div className="App">
+      <main className= "d-flex">
+      <div className= "date">{dateBuilder(new Date())}</div>
+        <div className= "search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."></input>
+        </div>
+        <div>
+          <div className= "location">New York City, US</div>
+        <div className = "row weather-box">
+          <div className= "col-sm">
+          <div className= "temp"> 20°C</div>
+          <div className="description">Sunny</div>
+          </div>
+          <div className= "col-sm"><img src = "icons/01d.png"></img></div>
+        </div>
+        <div>Sunset in 30 mins</div>
+        </div>
       </main>
-      <footer class="bg-light mt-auto py-3 text-center text-muted">- Coded by Miriam Said -</footer>
+      <footer className="bg-light mt-auto py-3 text-center text-muted"><small>- Coded by Miriam Said -</small></footer>
     </div>
   );
 }
