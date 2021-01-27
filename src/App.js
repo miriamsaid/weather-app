23b5229cc7a6e3e7ab383a63aeddb6b5919a3980
miriam said <miriamsaid.ms@gmail.com>
2021-01-27 16:18:46 +0000

    background

--- src/App.js
+++ src/App.js
@@ -3,33 +3,26 @@ import './App.css';
 import $ from "jquery";
 
 
-$(document).ready(function(){
-  var header = $('body');
-  
-  var backgrounds = new Array(
-    'url(desert.jpg)',
-    'url(country.jpg)',
-    'url(mountains.jpg)',
-    'url(orangesky.jpg)',
-    'url(trees.jpg)',
-    'url(beach.jpg)',
-  );
-  
-  var current = 0;
-  
-  function nextBackground() {
-      current++;
-      current = current % backgrounds.length;
-      header.css('background-image', backgrounds[current]);
-  }
-  setInterval(nextBackground, 1000);
-  
-  header.css('background-image', backgrounds[0]);
-  });
+var images = ["desert.jpg", "country.jpg", "mountains.jpg", "orangesky.jpg", "trees.jpg", "beach.jpg"];
+
+    $(function () {
+        var i = 0;
+        $("#dvImage").css("background-image", "url(images/" + images[i] + ")");
+        setInterval(function () {
+            i++;
+            if (i == images.length) {
+                i = 0;
+            }
+            $("#dvImage").fadeOut("slow", function () {
+                $(this).css("background-image", "url(images/" + images[i] + ")");
+                $(this).fadeIn("slow");
+            });
+        }, 5000);
+    });
 
 function App() {
   return (
-    <div className="App">
+    <div id="dvImage" className="App">
       <header className="App-header">
         <h1>Weather App</h1>
       </header>
